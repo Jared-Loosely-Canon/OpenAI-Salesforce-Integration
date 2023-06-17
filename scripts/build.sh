30ba67fd66e6aae0cc3fd66f1a76540d2372c725
Jared Simpson <jaredsimpson@looselycanon.com>
2023-06-17 11:43:32 -0400

    prod release

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,3 @@
-sfdx package:version:create -x --package "oai_sf_i" --wait 20 -v
+sfdx package:version:create -x --package "oai_sf_i" --wait 20 --targetdevhubusername jared@canonconnect.io --codecoverage
+sfdx package:version:create -x --package "oai_sf_i" --wait 20 -v jared@canonconnect.io --codecoverage
+sfdx package:version:promote --package "04tDn000000aEGYIA2"
